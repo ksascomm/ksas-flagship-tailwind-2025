ecbc0ff5a4ee246ce61279506bbe37c6d72cf7ec
ksascomm <ksasweb@jhu.edu>
2026-08-25 10:47:57 -0400

    Fix font name

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'FLAGSHIP_TAILWIND_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'FLAGSHIP_TAILWIND_VERSION', '4.1.1' );
+	define( 'FLAGSHIP_TAILWIND_VERSION', '4.1.2' );
 }
 
 if ( ! function_exists( 'flagship_tailwind_setup' ) ) :
